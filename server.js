7d13d0e0a2e7ee4b01cd2da1525a1d5df4784d83
François Jiang <francois.jiang@centrale-marseille.fr>
2021-10-25 23:22:08 +0200

    updating route redirection

--- server.js
+++ server.js
@@ -6,13 +6,11 @@ const app = express();
 const hostname = '127.0.0.1';
 const port = 3000;
 
-const urls = ['/', '/search', '/fight'];
-
 const monsters = require('./resources/monsters.json');
 
 app.use('/', express.static(path.join(__dirname, '/react/build/')));
 
-urls.map(url => app.get(url,  (req, res) => res.redirect(301, '/index.html')));
+app.get('/',  (req, res) => res.redirect(301, '/index.html'));
 
 app.get('/api/monster', (req, res) => {
     const id = req.query.id;
@@ -20,6 +18,8 @@ app.get('/api/monster', (req, res) => {
 
     res.send({"monster": monsters[id][level]});
 });
+
+app.get('*', (req, res) => res.redirect('/'));
 
 app.listen(port, hostname);
 console.log(`Server running at http://${hostname}:${port}/`);
